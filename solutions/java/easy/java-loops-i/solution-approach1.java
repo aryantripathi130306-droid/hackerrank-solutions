ea4f7ff089c12f33eb9a9a0edacebdf97745a783
Aryan Tripathi <aryantripathi130306@gmail.com>
2026-08-22 21:12:20 +0530

    ✅ Java Loops I [java] — approach1

---
+++ solutions/java/easy/java-loops-i/solution-approach1.java
@@ -0,0 +1,34 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-loops-i/problem?isFullScreen=true
+// Problem     Java Loops I
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-22, 09:12 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        for(int i =1; i<=10; i++){
+            int result=N*i;
+            System.out.println(N+" x "+i+" = "+result);
+        }
+    }
+}
